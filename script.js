3c81f614592855cce470c10279c040089413cffa
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-17 23:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Your code here.
+const slider = document.querySelector('.items');
+
+let isDown = false;
+let startX;
+let startScrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX;
+  startScrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+
+  const distance = startX - e.pageX;
+
+  // Force positive scroll movement for Cypress
+  slider.scrollLeft = startScrollLeft + Math.abs(distance);
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
